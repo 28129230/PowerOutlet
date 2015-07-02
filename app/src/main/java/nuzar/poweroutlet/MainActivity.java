86fdd760601a83ad8e194fdeb36821a0992611ef
28129230 <28129230@qq.com>
2015-07-02 11:45:45 +0800

    打开蓝牙
    打开蓝牙功能

--- app/src/main/java/nuzar/poweroutlet/MainActivity.java
+++ app/src/main/java/nuzar/poweroutlet/MainActivity.java
@@ -1,18 +1,58 @@
 package nuzar.poweroutlet;
 
+import android.app.Activity;
+import android.bluetooth.BluetoothA2dp;
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.bluetooth.BluetoothAdapter;
+import android.widget.Toast;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    private BluetoothAdapter mBluetoothAdapter = null;
+    private static final int REQUEST_ENABLE_BT = 1;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (mBluetoothAdapter == null) {
+            Toast.makeText(this, "Bluetooth is not available", Toast.LENGTH_LONG).show();
+            finish();
+            }
     }
+
+    @Override
+    public void onStart(){
+        super.onStart();
+        if(!mBluetoothAdapter.isEnabled()){
+            Intent enableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableIntent, REQUEST_ENABLE_BT);
+        }else{
+            Toast.makeText(this, "Bt_enable", Toast.LENGTH_SHORT).show();
+        }
+
+
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data){
+        switch (requestCode){
+            case REQUEST_ENABLE_BT:
+                if(resultCode == Activity.RESULT_OK){
+                    Toast.makeText(this, "Bt_enable", Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(this, R.string.bt_not_enabled_leaving, Toast.LENGTH_SHORT).show();
+                }
+        }
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
